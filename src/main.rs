3ff409a82cec0f5ba9cc948e20bac88dcc7f7e4c
4f48 <0x4f48@proton.me>
2024-07-26 17:24:44 +0200

    Parse TOML, make a request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{Client, Error, Response};
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Deserialize, Debug)]
+struct ConfigFile {
+    config: Config,
+    record: Record,
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    api_key: String,
+    secret_key: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Record {
+    domain: String,
+    rtype: RecordType,
+}
+
+#[derive(Deserialize, Debug)]
+enum RecordType {
+    A,
+    AAAA,
+    Both,
+}
+
+#[tokio::main]
+async fn main() {
+    let config =
+        std::fs::read_to_string("dpb.toml").expect("Couldn't read dpb.toml configuration file.");
+    let config: ConfigFile = toml::from_str(&config).expect("Failed to parse config.");
+
+    let client = Client::new();
+    let response = ping(&client, config.config.api_key, config.config.secret_key)
+        .await
+        .expect("Failed to ping.");
+    dbg!(response);
+}
+
+async fn ping(client: &Client, api_key: String, secret_key: String) -> Result<Response, Error> {
+    client
+        .post("https://api.porkbun.com/api/json/v3/ping")
+        .json(&json!({
+            "secretapikey": secret_key,
+            "apikey": api_key
+        }))
+        .send()
+        .await
 }
